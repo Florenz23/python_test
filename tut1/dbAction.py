9fb2766220a8e289ec3ecbcfa2b3f4adfac41e12
lama23 <florenz.erstling@lalalama.de>
2018-01-24 23:41:27 +0100

    ini commit

---
+++ tut1/dbAction.py
@@ -0,0 +1,44 @@
+from insertDataFunction import insert_vendor
+from insertDataFunction import insert_vendor_list
+from config import config
+import psycopg2
+
+
+
+def insertData():
+    insert_vendor("3M Co.")
+    # insert multiple vendors
+    insert_vendor_list([
+        ('AKM Semiconductor Inc.',),
+        ('Asahi Glass Co Ltd.',),
+        ('Daikin Industries Ltd.',),
+        ('Dynacast International Inc.',),
+        ('Foster Electric Co. Ltd.',),
+        ('Murata Manufacturing Co. Ltd.',)
+    ])
+
+def get_vendors():
+    """ query data from the vendors table """
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute("SELECT vendor_id, vendor_name FROM vendors ORDER BY vendor_name")
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+
+get_vendors()
